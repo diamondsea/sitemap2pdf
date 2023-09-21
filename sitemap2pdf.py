8fd86cba18f27370f5ef55006db048263d2b3cd8
Brian Gallagher <github@diamondsea.com>
2023-09-22 00:06:23 +0100

    save multiple pages and pdfs

--- sitemap2pdf.py
+++ sitemap2pdf.py
@@ -1,54 +1,64 @@
 import requests
-from bs4 import BeautifulSoup
-from selenium import webdriver
-import pdfkit
-import argparse
+import sys
 import time
+from bs4 import BeautifulSoup
+import pdfkit
+from PyPDF2 import PdfMerger
 
-def get_links_from_sitemap(sitemap_url):
-    r = requests.get(sitemap_url)
-    soup = BeautifulSoup(r.text, "xml")
+def parse_sitemap(sitemap_url):
+    resp = requests.get(sitemap_url)
+    soup = BeautifulSoup(resp.content, 'xml')
     
-    links = []
-    
-    for loc in soup.find_all("loc"):
-        links.append(loc.text)
-        
-    for url in soup.find_all("url"):
-        links.append(url.loc.text)
-    
-    return links
+    # Check if it's sitemap or href format
+    urls = [loc.text for loc in soup.find_all('loc')]
+    if not urls:
+        urls = [link.get('href') for link in soup.find_all('link') if link.get('href')]
+    return urls
 
-def render_and_convert_to_pdf(url):
+def convert_links_to_pdf(urls, output_filename, hide_classes=None, num_links=None, start_offset=0):
+    if hide_classes is None:
+        hide_classes = []
+    
     options = {
+        'no-outline': None,
         'quiet': ''
     }
-    rendered_content = pdfkit.from_url(url, False, options=options)
-    return rendered_content
-
-def main():
-    parser = argparse.ArgumentParser(description='Convert links from sitemap.xml to PDF.')
-    parser.add_argument('sitemap_url', type=str, help='URL of the sitemap.xml')
-    parser.add_argument('output_filename', type=str, help='Name of the output PDF file')
-    parser.add_argument('--num_links', type=int, default=10, help='Number of links to convert')
-    parser.add_argument('--start_offset', type=int, default=0, help='Starting offset of links to convert')
-
-    args = parser.parse_args()
-
-    links = get_links_from_sitemap(args.sitemap_url)
     
-    start_time = time.time()
-    with open(args.output_filename, 'wb') as f:
-        for index, link in enumerate(links[args.start_offset:args.start_offset + args.num_links]):
-            pdf_content = render_and_convert_to_pdf(link)
-            f.write(pdf_content)
-            
-            elapsed_time = time.time() - start_time
-            estimated_time = elapsed_time / (index + 1) * args.num_links - elapsed_time
-            progress = (index + 1) / args.num_links * 100
-            
-            print(f"{index + 1} of {args.num_links} ({progress:.2f}%) completed, Remaining {estimated_time:.2f} seconds, getting {link}")
+    if hide_classes:
+        hide_js = ",".join([f"document.querySelectorAll('.{cls}').forEach(e => e.style.display = 'none');" for cls in hide_classes])
+        options['javascript-delay'] = 1000
+        options['run-script'] = hide_js
 
-if __name__ == "__main__":
-    main()
+    merged_pdf = PdfMerger()
+    total_urls = len(urls)
+    num_links = num_links or total_urls
+    end_offset = start_offset + num_links
+    urls_subset = urls[start_offset:end_offset]
+    
+    for index, url in enumerate(urls_subset, 1):
+        start_time = time.time()
+        print(f"{index + start_offset} of {total_urls} ({(index/num_links)*100:.2f}%) completed, getting {url}")
+        try:
+            pdf_path = url.replace("https://", "").replace("http://", "").replace("/", "_") + ".pdf"
+            pdfkit.from_url(url, pdf_path, options=options)
+            merged_pdf.append(pdf_path)
+        except Exception as e:
+            print(f"Error processing {url}: {e}")
+        
+        elapsed_time = time.time() - start_time
+        estimated_remaining = (num_links - index) * elapsed_time
+        print(f"Remaining {estimated_remaining/60:.2f} minutes")
+    
+    merged_pdf.write(output_filename)
+    merged_pdf.close()
+
+if __name__ == '__main__':
+    sitemap_url = sys.argv[1]
+    output_filename = sys.argv[2]
+    num_links = int(sys.argv[3])
+    start_offset = int(sys.argv[4])
+    hide_classes = sys.argv[5:]
+    
+    urls = parse_sitemap(sitemap_url)
+    convert_links_to_pdf(urls, output_filename, hide_classes=hide_classes, num_links=num_links, start_offset=start_offset)
 
